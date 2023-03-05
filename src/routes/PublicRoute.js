7d99cded5a67ae9777b79bd42660b00c231aecb8
Sazzad Hossain <piash@Pishu.local>
2023-03-05 16:33:58 +0600

    First commit

---
+++ src/routes/PublicRoute.js
@@ -0,0 +1,17 @@
+import Main from "../layout/Main";
+import Home from "../pages/Home/Home";
+
+const { createBrowserRouter } = require("react-router-dom");
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    }
+])
